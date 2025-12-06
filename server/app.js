872652aacf0a54fc9c29e2e45a46eadc2681deef
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2025-12-06 08:55:12 +0530

    feat : backend added

--- server/app.js
+++ server/app.js
@@ -1,10 +1,39 @@
 const express = require("express");
+const connectDB = require("./src/config/database");
 const cookieParser = require("cookie-parser");
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config({});
 const cors = require("cors");
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on ` + process.env.PORT)
-})
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+//routes
+const authRouter = require("./src/routes/auth.routes");
+const profileRouter = require("./src/routes/profile.routes");
+const requestRouter = require("./src/routes/request.routes");
+const userRouter = require("./src/routes/user.routes");
+
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/", userRouter);
+
+//database connect before server
+connectDB().then(() => {
+  try {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on ` + process.env.PORT);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
